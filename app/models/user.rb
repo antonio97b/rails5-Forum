417ee80dd884c13d4c8114c165ef6aed515f58da
Tony <tony.navarro@gmail.com>
2016-11-16 22:03:47 -0800

    - Create a find_all_user_comments method. - Implement that method in a basic form on the control panel route (WIP).

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
 
   has_many :forums
   has_many :posts
+
+  def self.find_all_user_comments(user)
+    @comment_hash = []
+    @user_comments = Comment.where(user_id: user.id)
+    @user_comments.all.each do |co|
+      post = Post.find(co.commentable_id)
+      @comment_hash.push( { comment: co.body, post: post.title} )
+    end
+    @comment_hash
+  end
 end
