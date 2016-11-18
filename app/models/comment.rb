68c3ca7526bb902faf7225ccf465fa68173a6205
Tony <tony.navarro@gmail.com>
2016-11-17 18:14:10 -0800

    - Refactor the find_all_user_comments so that it works on all :commentable_types. - Render a table for the user's most recent comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,9 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentable
+
+  def find_parent_post
+    return self.commentable if self.commentable.is_a?(Post)
+    self.commentable.find_parent_post
+  end
 end
