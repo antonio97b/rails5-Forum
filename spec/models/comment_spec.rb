206fd552db30d263557bfa52ec0b0505efd45c1f
Tony <tony.navarro@gmail.com>
2016-11-24 12:13:39 -0800

    - Alter tests to accept Comment required :user_id.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -11,14 +11,14 @@ RSpec.describe Comment, type: :model do
   end
 
   it "should allow a Comment to respond to a post" do
-    comment = @post.comments.create!(body: "Testing commenting on a Post.")
+    comment = @post.comments.create!(body: "Testing commenting on a Post.", user_id: @user.id)
 
     expect(@post.comments).to eq([comment])
   end
 
   it "should allow a Comment to respond to another Comment." do
-    comment1 = @post.comments.create!(body: "test comment.")
-    comment2 =  comment1.comments.create!(body: "Comment response to test body.")
+    comment1 = @post.comments.create!(body: "test comment.", user_id: @user.id)
+    comment2 =  comment1.comments.create!(body: "Comment response to test body.", user_id: @user.id)
 
     expect(comment1.comments).to eq([comment2])
   end
