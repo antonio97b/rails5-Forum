31380702f3d09fecd85216ca654dbc18ea5de4e0
Tony <tony.navarro@gmail.com>
2016-12-01 16:44:23 -0800

    - Prevent commenting on deleted comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -28,7 +28,7 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to post_path(@comment.commentable_id) }
+        format.html { redirect_to post_path(params[:post_id]) }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
